52f943040c95b0a4943e5d207ea166b0f511a632
Rodrigo Mariano <rodmariano13@gmail.com>
2021-02-19 08:32:25 -0300

    change configs

--- app.py
+++ app.py
@@ -9,25 +9,28 @@ app = Flask(__name__)
 
 @app.route("/add")
 def add():
+    # curl "localhost:5000/add?first_num=3&second_num=2"
     print('\nadd()')
 
     first_num = request.args.get('first_num')
     second_num = request.args.get('second_num')
 
-    task = add_nums.delay(first_num, second_num)
+    # task = add_nums.delay(first_num, second_num)
+    task = add_nums.apply_async((first_num, second_num), countdown=5)
 
     print('first_num: ', first_num)
     print('second_num: ', second_num)
     print('task: ', task)
     print('state: ', task.state)
     print('result: ', task.result)
-    print('get(): ', task.get())
+    print('get(): ', task.get()) # <-- it does not work
 
     return jsonify({'result': task.result}), 200
 
 
 @app.route("/subtract")
 def subtract():
+    # curl "localhost:5000/subtract?first_num=3&second_num=2"
     print('\nsubtract()')
 
     first_num = request.args.get('first_num')
--- worker_b.py
+++ worker_b.py
@@ -1,7 +1,8 @@
 from celery import Celery
 
 # Celery configuration
-CELERY_BROKER_URL = 'amqp://guest:guest@tfcd_rabbit:5672/'
+CELERY_BROKER_URL = 'amqp://guest:guest@tfcd_rabbit:5672//'
+# CELERY_BROKER_URL = 'pyamqp://guest@tfcd_rabbit//'
 CELERY_RESULT_BACKEND = 'rpc://'
 
 # Initialize Celery
